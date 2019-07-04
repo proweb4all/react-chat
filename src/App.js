1c0d6bff632a02d8d80b6e0fad74404d61d1d2f9
proweb4all <proweb4all@narod.ru>
2019-07-04 14:59:55 +0300

    start app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './welcome';
-import Clock from './clock';
-import List from './list';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Welcome name='Yury'>Taratov</Welcome>
-        <hr />
-        <Clock />
-        <hr />
-        <List />
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Chat</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/welcome.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Welcome = ({name, children}) => {
-    return (
-        <h1>{`Hello, ${name} ${children}`}</h1>
-    )
-}
-export default Welcome;
